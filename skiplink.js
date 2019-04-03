35071e167e7d249947f44898838231bd33b215e8
Jason Morris <jasonmorris@gmail.com>
2019-04-03 10:44:44 -0400

    add IIFE and eslint rule adjustments

--- skiplink.js
+++ skiplink.js
@@ -1,58 +1,65 @@
-var historySupport = Boolean(history && history.pushState);
-var extraPadding = 100;
-var validHash = /^#[a-z0-9%_-]+$/g;
+(function skiplink() {
+  "use strict";
 
-window.addEventListener("DOMContentLoaded", skipNow);
-window.addEventListener("hashchange", skipNow);
-document.body.addEventListener("click", linkLimiter);
+  var historySupport = Boolean(history && history.pushState);
+  var extraPadding = 100;
+  var validHash = /^#[a-z0-9%_-]+$/g;
 
-function skipNow() {
-  skipLink(window.location.hash, false);
-}
+  window.addEventListener("DOMContentLoaded", skipNow);
+  window.addEventListener("hashchange", skipNow);
+  document.body.addEventListener("click", linkLimiter);
 
-function skipLink(target, pushHistory) {
-  var targetElement;
-
-  if (!validHash.test(target)) {
-    return false;
+  function skipNow() {
+    skipLink(window.location.hash, false);
   }
 
-  targetElement = document.getElementById(target.slice(1));
+  function skipLink(target, pushHistory) {
+    var targetElement;
 
-  if (targetElement) {
-    window.scrollTo(window.pageXOffset, yPadded(targetElement, extraPadding));
-    fullFocus(targetElement);
+    if (!validHash.test(target)) {
+      return false;
+    }
+
+    targetElement = document.getElementById(target.slice(1));
+
+    if (targetElement) {
+      window.scrollTo(window.pageXOffset, yPadded(targetElement, extraPadding));
+      fullFocus(targetElement);
+    }
+
+    if (historySupport && pushHistory) {
+      history.pushState({}, document.title, location.pathname + target);
+    }
+
+    return Boolean(targetElement);
   }
 
-  if (historySupport && pushHistory) {
-    history.pushState({}, document.title, location.pathname + target);
+  function linkLimiter(event) {
+    var source = event.target;
+
+    if (
+      source.nodeName === "A" &&
+      skipLink(source.getAttribute("href"), true)
+    ) {
+      event.preventDefault();
+    }
   }
 
-  return Boolean(targetElement);
-}
-
-function linkLimiter(event) {
-  var source = event.target;
-
-  if (source.nodeName === "A" && skipLink(source.getAttribute("href"), true)) {
-    event.preventDefault();
+  function fullFocus(element) {
+    if (!(/^(?:a|select|input|button|textarea)$/i).test(element.tagName)) {
+      element.setAttribute("tabindex", -1);
+      element.addEventListener("blur", removeTabindex, true);
+    }
+    element.focus();
   }
-}
 
-function fullFocus(element) {
-  if (!(/^(?:a|select|input|button|textarea)$/i).test(element.tagName)) {
-    element.setAttribute("tabindex", -1);
-    element.addEventListener("blur", removeTabindex, true);
+  function removeTabindex(event) {
+    event.target.removeAttribute("tabindex");
   }
-  element.focus();
-}
 
-function removeTabindex(event) {
-  event.target.removeAttribute("tabindex");
-}
+  function yPadded(element, padding) {
+    var elementPosition = element.getBoundingClientRect();
 
-function yPadded(element, padding) {
-  var elementPosition = element.getBoundingClientRect();
-
-  return window.pageYOffset + elementPosition.top - padding;
-}
+    return window.pageYOffset + elementPosition.top - padding;
+  }
+}());
